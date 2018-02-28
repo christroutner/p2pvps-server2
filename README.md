1560fec2ca7a739d9bac9fc297fd14e35eb57ad4
Chris Troutner <chris.troutner@gmail.com>
2018-02-28 14:57:49 -0800

    Beginning to implement mocha tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # P2P VPS (Server 2)
 This is the second incarnation of the P2P VPS Server. It's slimmed down and drops
 the usage of ConnextCMS, opting instead to customize directly on top of KeystoneJS.
+
+[![Build Status](https://travis-ci.org/christroutner/p2pvps-server2.svg?branch=master)](https://travis-ci.org/christroutner/p2pvps-server2)
---
+++ test/integration/user.test.js
@@ -0,0 +1,94 @@
+/*
+  This is a mocha integration test of the strap api.
+*/
+
+const rp = require('request-promise');
+const assert = require('chai').assert;
+const user = {};
+
+before(function (done) {
+
+});
+
+after(function (done) {
+
+});
+
+describe('User', function() {
+
+  describe('POST /auth/local/register - Create Test User', function() {
+    it('should return 200 status code', async () => {
+
+      let options = {
+        method: 'POST',
+        uri: strapi.config.url+'/auth/local/register',
+        resolveWithFullResponse: true,
+        json: true,
+        body: {
+          _id: null,
+          username: 'Test User',
+          email: 'test@test.com',
+          password: '123456'
+        }
+      };
+      //let result = await rp(options);
+      //console.log(`result stringified: ${JSON.stringify(result,null,2)}`);
+      //assert.equal(result.statusCode, 200);
+      //assert(result.statusCode === 200 || results.statusCode === 400, 'Creates new or reports already exists.');
+      assert(1,1, 'Testing the test.');
+    });
+  });
+
+  /*
+  describe('POST /auth/local - Log in as user', function() {
+    it('should return 200 status code', async () => {
+
+      try {
+        let options = {
+          method: 'POST',
+          uri: strapi.config.url+'/auth/local',
+          resolveWithFullResponse: true,
+          json: true,
+          body: {
+            identifier: 'test@test.com',
+            password: '123456'
+          }
+        };
+
+        let result = await rp(options);
+        console.log(`result stringified: ${JSON.stringify(result,null,2)}`);
+        user.id = result.body.user._id;
+        user.jwt = result.body.jwt;
+        //assert.equal(result.statusCode, 200);
+        assert(result.statusCode === 200 || results.statusCode === 400, 'Creates new or reports already exists.');
+      } catch(err) {
+        console.error('Error caught and stringified: '+JSON.stringify(err,null,2));
+      }
+    });
+  });
+
+
+  describe('DELETE /user/:id', function() {
+    it('should return 401 status code', async () => {
+
+      //console.log(`strapi plugins: ${JSON.stringify(strapi.plugins,null,2)}`);
+      console.log(`UserId: ${user.id}`);
+
+      let options = {
+        method: 'DELETE',
+        uri: strapi.config.url+`/user/${user.id}`,
+        resolveWithFullResponse: true,
+        //json: true,
+        //body: {
+        //}
+        headers: {
+          Authorization: `Bearer ${user.jwt}`
+        }
+      };
+      let result = await rp(options);
+      console.log(`result stringified: ${JSON.stringify(result,null,2)}`);
+      assert.equal(result.statusCode, 401);
+    });
+  });
+  */
+});
